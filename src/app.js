6c9eb926f641d472977e1efb1e532de4a15f0f08
Alex Langshur <adlangshur@gmail.com>
2020-02-19 13:54:40 -0800

    Added browser routing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-    return (
-        <div className="App">
-            <p>
-                Universal plug.
-            </p>
-        </div>
-    );
+import HomePage from './components/home';
+import AuctionPage from './components/auction';
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/auction' component={AuctionPage} exact />
+                    <Route path='/' component={HomePage} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/auction/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AuctionPage extends Component {
+    render() {
+        return (
+            <div>
+                Auction Page
+            </div>
+        );
+    }   
+}
+
+export default AuctionPage;
