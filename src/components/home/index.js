fc0e8af16a39d45474336e5565d098c47c609802
Alex Langshur <adlangshur@gmail.com>
2020-02-19 23:17:56 -0800

    Upgraded profile icon

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,148 +2,125 @@ import React, { Component } from 'react';
 
 import TestImage from '../../assets/test.png';
 import BackgroundImage from '../../assets/background.png';
+import InstagramIcon from '../../assets/instagram.png';
+import YoutubeIcon from '../../assets/youtube.png';
+import WebsiteIcon from '../../assets/website.png';
+import EmailIcon from '../../assets/email.png';
+import LinkedInIcon from '../../assets/linkedin.png';
 
 class HomePage extends Component {
     render() {
         return (
+            <>
 
-            <div
-                draggable={false}
-                style={{
-                    position: 'absolute',
-                    display: 'flex',
-
-                    top: 0,
-                    left: 0,
-
-                    height: '100%',
-                    width: '100%',
-
-                    justifyContent: 'center',
-                    alignItems: 'center',
-
-                    backgroundImage: `url(${BackgroundImage})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center center',
-                    backgroundRepeat: 'no-repeat',
-                    fontFamily: 'Arial, Helvetica, sans-serif'
-                }}
-            >
-
-                {/* profile container */}
+                {/* page canvas */}
                 <div
-                    id='profile'
+                    draggable={false}
                     style={{
-                        position: 'relative',
+                        position: 'absolute',
                         display: 'flex',
 
-                        height: '40%',
-                        width: '40%',
-
                         justifyContent: 'center',
-                        alignItems: 'center'
+                        alignItems: 'center',
+
+                        top: 0,
+                        left: 0,
+                        height: '100%',
+                        width: '100%',
+
+                        backgroundImage: `url(${BackgroundImage})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center center',
+                        backgroundRepeat: 'no-repeat',
+
+                        cursor: 'default',
+                        fontFamily: 'Helvetica'
                     }}
                 >
 
-                    {/* profile image */}
-                    <img
-                        id='profile-image'
-                        src={TestImage}
-                        alt='Test Image'
-                        draggable={false}
-                        onLoad={this.onImgLoad}
-
-                        style={{
-                            width: '100%',
-                            borderRadius: '10px',
-                            objectFit: 'cover'
-                        }}
-                    />
-
-                    {/* profile text */}
+                    {/* page cutout */}
                     <div
-                        id='profile-text'
                         style={{
-                            position: 'absolute',
                             display: 'flex',
-                            zIndex: 1,
 
-                            width: '30%',
-
-                            right: '105%',
-                            top: '65%',
-
-                            padding: '15px',
-
-                            justifyContent: 'center',
                             alignItems: 'center',
+                            flexDirection: 'column',
 
-                            borderColor: 'black',
-                            borderWidth: '1px',
-                            borderStyle: 'solid',
-                            borderRadius: '4px',
+                            height: '90%',
+                            width: '500px',
 
-                            backgroundColor: '#f6f6f6',
-                            fontSize: '0.9vw'
+                            backgroundImage: 'linear-gradient(#ffcccb, white)',
+                            borderRadius: '60px',
+                            boxShadow: '0 0 10px lightgrey'
                         }}
                     >
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                        Test profile text
-                    </div>
 
-                    {/* profile links */}
-                    <div
-                        id='profile-links'
-                        style={{
-                            position: 'absolute',
-                            display: 'flex',
-                            zIndex: 1,
+                        {/* profile image */}
+                        <img 
+                            src={TestImage}
+                            draggable={false}
+                            style={{
+                                marginTop: '13%',
+                                height: '275px',
+                                width: '275x',
 
-                            width: '30%',
+                                borderRadius: '10px',
+                                boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+                            }}
+                        />
 
-                            left: '105%',
-                            top: '5%',
+                        {/* profile title */}
+                        <div
+                            style={{
+                                marginTop: '8%',
 
-                            padding: '15px',
+                                fontSize: '28px',
+                                fontWeight: 'bold',
+                                letterSpacing: '2px',
+                                userSelect: 'none',
+                                msUserSelect: 'none',
+                                KhtmlUserSelect: 'none',
+                                MozUserSelect: 'none'
+                            }}
+                        >
+                            Grace Sherman
+                        </div>
 
-                            justifyContent: 'center',
-                            alignItems: 'center',
+                        {/* profile links */}
+                        <div
+                            style={{
+                                marginTop: '5%'
+                            }}
+                        >
+                            <img  src={InstagramIcon} draggable={false} style={{ height: '25px', width: '25px', opacity: '0.55', paddingLeft: '7px', paddingRight: '7px', cursor: 'pointer' }} />
+                            <img  src={YoutubeIcon} draggable={false} style={{ height: '25px', width: '25px', opacity: '0.55', paddingLeft: '7px', paddingRight: '7px', cursor: 'pointer' }} />
+                            <img  src={WebsiteIcon} draggable={false} style={{ height: '25px', width: '25px', opacity: '0.55', paddingLeft: '7px', paddingRight: '7px', cursor: 'pointer' }} />
+                            <img  src={EmailIcon} draggable={false} style={{ height: '25px', width: '25px', opacity: '0.55', paddingLeft: '7px', paddingRight: '7px', cursor: 'pointer' }} />
+                            <img  src={LinkedInIcon} draggable={false} style={{ height: '25px', width: '25px', opacity: '0.55', paddingLeft: '7px', paddingRight: '7px', cursor: 'pointer' }} />
+                        </div>
 
-                            borderColor: 'black',
-                            borderWidth: '1px',
-                            borderStyle: 'solid',
-                            borderRadius: '4px',
+                        {/* profile text */}
+                        <div
+                            style={{
+                                marginTop: '5%',
+                                padding: '40px',
 
-                            backgroundColor: '#f6f6f6',
-                            fontSize: '0.9vw'
-                        }}
-                    >
-                        Test profile links
-                        Test profile links
-                        Test profile links
-                        Test profile links
-                        Test profile links
-                        Test profile links
-                        Test profile links
-                        Test profile links
+                                textAlign: 'center',
+                                fontSize: '15px',
+                                color: '#36454F',
+                                lineHeight: '20px',
+                                userSelect: 'none',
+                                msUserSelect: 'none',
+                                KhtmlUserSelect: 'none',
+                                MozUserSelect: 'none'
+                            }}
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet egestas felis. Mauris dignissim augue diam, nec ultrices purus lobortis eu. Integer quis scelerisque diam, et fringilla enim. Cras nibh ipsum, pulvinar non orci at, efficitur aliquet eros. Donec vitae lorem nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur eget euismod mauris. Vivamus tincidunt vulputate posuere. In euismod euismod lectus, nec sodales sem faucibus sit amet. Sed ut viverra libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vitae nisl accumsan, porta magna vitae, dapibus quam. Sed interdum ante enim, eget iaculis nibh ornare non. Quisque ornare vestibulum eros vel bibendum. Suspendisse libero dui, cursus eget mi blandit, mattis dictum turpis. Curabitur interdum tincidunt diam, ut luctus justo.
+                        </div>
+
                     </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
